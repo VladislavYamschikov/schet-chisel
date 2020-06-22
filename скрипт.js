48b0b324ce7e2bb9498daeca204ab792a304e6e2
VladislavYamschikov <62208786+VladislavYamschikov@users.noreply.github.com>
2020-06-22 14:52:31 +0300

    final

---
+++ скрипт.js
@@ -0,0 +1,13 @@
+
+		function addition() {
+			var a = parseInt(document.getElementById('a').value);
+			var b = parseInt(document.getElementById('b').value);
+ 
+			if (isNaN(a)==true) a=0;
+			if (isNaN(b)==true) b=0;
+ 
+			var c = a + b;
+ 
+			document.getElementById('result').innerHTML =  c;
+		}
+		
